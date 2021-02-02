6a02a4fd60d48ed56b097ea9872b766b56af763a
Sümeyye Karatekin <sumeyyekaratekin96@gmail.com>
2021-02-02 16:46:02 +0300

    Add project files.

---
+++ Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carmanager = new CarManager(new InMemoryProductDal());
+            foreach (var car in carmanager.GetAll())
+            {
+                Console.WriteLine("Brand : "+car.BrandId+" Daily Price :" +car.DailyPrice+" TL");
+            }
+        }
+    }
+}
