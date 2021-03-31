f5af24419f1adf8b1d2728febcb2e8aee854e98c
sumeyyekaratekin <sumeyyekaratekin96@gmail.com>
2021-03-31 22:50:01 +0300

    Database Updated.

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,104 +1,231 @@
-﻿CREATE TABLE [dbo].[Brands] (
-    [Id]        INT           IDENTITY (1, 1) NOT NULL,
-    [BrandName] NVARCHAR (50) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
-CREATE TABLE [dbo].[CarImages] (
-    [Id]        INT            IDENTITY (1, 1) NOT NULL,
-    [CarId]     INT            NOT NULL,
-    [ImagePath] NVARCHAR (MAX) NOT NULL,
-    [Date]      DATETIME       NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([CarId]) REFERENCES [dbo].[Cars] ([Id])
-);
-
-CREATE TABLE [dbo].[Cars] (
-    [Id]              INT            IDENTITY (1, 1) NOT NULL,
-    [CarName]         NVARCHAR (50)  NOT NULL,
-    [BrandId]         INT            NOT NULL,
-    [ColorId]         INT            NOT NULL,
-    [ModelYear]       INT            NOT NULL,
-    [DailyPrice]      DECIMAL (18)   NOT NULL,
-    [Description]     NVARCHAR (250) NOT NULL,
-    [MinFindeksScore] INT            NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([ColorId]) REFERENCES [dbo].[Colors] ([Id]),
-    FOREIGN KEY ([BrandId]) REFERENCES [dbo].[Brands] ([Id])
-);
-
-CREATE TABLE [dbo].[Colors] (
-    [Id]        INT           IDENTITY (1, 1) NOT NULL,
-    [ColorName] NVARCHAR (25) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
-CREATE TABLE [dbo].[Customers] (
+﻿USE [RentACar]
+GO
+/****** Object:  Table [dbo].[Brands]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Brands](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[CarImages]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[CarImages](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CarId] [int] NULL,
+	[ImagePath] [varchar](255) NULL,
+	[Date] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Cars]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Cars](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[BrandId] [int] NULL,
+	[ColorId] [int] NULL,
+	[ModelYear] [int] NULL,
+	[CarName] [varchar](255) NULL,
+	[DailyPrice] [decimal](18, 0) NULL,
+	[Description] [varchar](255) NULL,
+	[MinFindeksScore] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Colors]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Colors](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](255) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[CustomerCards]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[CustomerCards](
+	[CustomerId] [int] NOT NULL,
+	[CardId] [int] NOT NULL,
+ CONSTRAINT [PK_CustomerCards] PRIMARY KEY CLUSTERED 
+(
+	[CustomerId] ASC,
+	[CardId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Customers]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customers](
 	[UserId] [int] NOT NULL,
 	[PhoneNumber] [varchar](15) NULL,
 	[Address] [varchar](150) NULL,
 	[FindeksScore] [int] NULL,
-    PRIMARY KEY CLUSTERED ([UserId] ASC),
-    FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
-);
-
-CREATE TABLE [dbo].[OperationClaims] (
-    [Id]   INT           IDENTITY (1, 1) NOT NULL,
-    [Name] VARCHAR (250) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
-CREATE TABLE [dbo].[Rentals] (
-    [Id]            INT      IDENTITY (1, 1) NOT NULL,
-    [CarId]         INT      NOT NULL,
-    [UserId]    INT      NOT NULL,
-    [RentDate]      DATETIME NOT NULL,
-    [ReturnDate]    DATETIME NOT NULL,
-    [RentStartDate] DATETIME NULL,
-    [RentEndDate]   DATETIME NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    FOREIGN KEY ([CarId]) REFERENCES [dbo].[Cars] ([Id]),
-    FOREIGN KEY ([UserId]) REFERENCES [dbo].[Customers] ([UserId])
-);
-
-CREATE TABLE [dbo].[UserOperationClaims] (
-    [Id]               INT IDENTITY (1, 1) NOT NULL,
-    [UserId]           INT NOT NULL,
-    [OperationClaimId] INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
-
-CREATE TABLE [dbo].[Users] (
-    [Id]           INT             IDENTITY (1, 1) NOT NULL,
-    [FirstName]    VARCHAR (50)    NOT NULL,
-    [LastName]     VARCHAR (50)    NOT NULL,
-    [Email]        VARCHAR (50)    NOT NULL,
-    [PasswordHash] VARBINARY (500) NOT NULL,
-    [PasswordSalt] VARBINARY (500) NOT NULL,
-    [Status]       BIT             NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
-
-CREATE TABLE [dbo].[FakeCard] (
-    [Id]             INT            IDENTITY (1, 1) NOT NULL,
-    [NameOnTheCard]  NVARCHAR (150) NOT NULL,
-    [CardNumber]     NVARCHAR (16)  NOT NULL,
-    [CardCvv]        NCHAR (3)      NOT NULL,
-    [expirationDate] NVARCHAR (50)  NOT NULL,
-    [MoneyInTheCard] DECIMAL (18)   NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
-CREATE TABLE [dbo].[CustomerCard] (
-    [CardId] INT NOT NULL,
-    [CustomerId]     INT NOT NULL,
-    PRIMARY KEY CLUSTERED ([CardId] ASC),
-    FOREIGN KEY ([CardId]) REFERENCES [dbo].[FakeCard] ([Id]),
-	FOREIGN KEY ([CustomerId]) REFERENCES [dbo].[Customers] ([UserId])
-);
-
-
-
-
+PRIMARY KEY CLUSTERED 
+(
+	[UserId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[FakeCards]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[FakeCards](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[NameOnTheCard] [varchar](255) NOT NULL,
+	[CardNumber] [varchar](16) NOT NULL,
+	[CardCvv] [varchar](3) NOT NULL,
+	[ExpirationDate] [varchar](5) NOT NULL,
+	[MoneyInTheCard] [decimal](18, 0) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[OperationClaims]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OperationClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](250) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Rentals]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Rentals](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[CarId] [int] NULL,
+	[UserId] [int] NULL,
+	[RentDate] [datetime] NULL,
+	[RentStartDate] [datetime] NULL,
+	[RentEndDate] [datetime] NULL,
+	[ReturnDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[UserOperationClaims]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserOperationClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [int] NOT NULL,
+	[OperationClaimId] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 15.03.2021 20:01:34 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [varchar](50) NOT NULL,
+	[LastName] [varchar](50) NOT NULL,
+	[Email] [varchar](100) NOT NULL,
+	[PasswordSalt] [varbinary](500) NOT NULL,
+	[PasswordHash] [varbinary](500) NOT NULL,
+	[Status] [bit] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Rentals] ADD  DEFAULT (NULL) FOR [RentEndDate]
+GO
+ALTER TABLE [dbo].[Rentals] ADD  DEFAULT (NULL) FOR [ReturnDate]
+GO
+ALTER TABLE [dbo].[CarImages]  WITH CHECK ADD  CONSTRAINT [FK_CarImages_Cars] FOREIGN KEY([CarId])
+REFERENCES [dbo].[Cars] ([Id])
+GO
+ALTER TABLE [dbo].[CarImages] CHECK CONSTRAINT [FK_CarImages_Cars]
+GO
+ALTER TABLE [dbo].[Cars]  WITH CHECK ADD  CONSTRAINT [FK_Cars_Brands] FOREIGN KEY([BrandId])
+REFERENCES [dbo].[Brands] ([Id])
+GO
+ALTER TABLE [dbo].[Cars] CHECK CONSTRAINT [FK_Cars_Brands]
+GO
+ALTER TABLE [dbo].[Cars]  WITH CHECK ADD  CONSTRAINT [FK_Cars_Colors] FOREIGN KEY([ColorId])
+REFERENCES [dbo].[Colors] ([Id])
+GO
+ALTER TABLE [dbo].[Cars] CHECK CONSTRAINT [FK_Cars_Colors]
+GO
+ALTER TABLE [dbo].[CustomerCards]  WITH CHECK ADD  CONSTRAINT [FK_CustomerCards_Customers] FOREIGN KEY([CustomerId])
+REFERENCES [dbo].[Customers] ([UserId])
+GO
+ALTER TABLE [dbo].[CustomerCards] CHECK CONSTRAINT [FK_CustomerCards_Customers]
+GO
+ALTER TABLE [dbo].[CustomerCards]  WITH CHECK ADD  CONSTRAINT [FK_CustomerCards_FakeCards] FOREIGN KEY([CardId])
+REFERENCES [dbo].[FakeCards] ([Id])
+GO
+ALTER TABLE [dbo].[CustomerCards] CHECK CONSTRAINT [FK_CustomerCards_FakeCards]
+GO
+ALTER TABLE [dbo].[Customers]  WITH CHECK ADD  CONSTRAINT [FK_Customers_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[Customers] CHECK CONSTRAINT [FK_Customers_Users]
+GO
+ALTER TABLE [dbo].[Rentals]  WITH CHECK ADD  CONSTRAINT [FK_Rentals_Cars] FOREIGN KEY([CarId])
+REFERENCES [dbo].[Cars] ([Id])
+GO
+ALTER TABLE [dbo].[Rentals] CHECK CONSTRAINT [FK_Rentals_Cars]
+GO
+ALTER TABLE [dbo].[UserOperationClaims]  WITH CHECK ADD  CONSTRAINT [FK_UserOperationClaims_OperationClaims] FOREIGN KEY([OperationClaimId])
+REFERENCES [dbo].[OperationClaims] ([Id])
+GO
+ALTER TABLE [dbo].[UserOperationClaims] CHECK CONSTRAINT [FK_UserOperationClaims_OperationClaims]
+GO
+ALTER TABLE [dbo].[UserOperationClaims]  WITH CHECK ADD  CONSTRAINT [FK_UserOperationClaims_Users] FOREIGN KEY([UserId])
+REFERENCES [dbo].[Users] ([Id])
+GO
+ALTER TABLE [dbo].[UserOperationClaims] CHECK CONSTRAINT [FK_UserOperationClaims_Users]
+GO
